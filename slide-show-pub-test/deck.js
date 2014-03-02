02a6ce47b3f1eee2f16a89539b840f3c5e17d1f6
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 23:07:57 -0800

    Show & Tell commit

---
+++ slide-show-pub-test/deck.js
@@ -0,0 +1,24 @@
+var deck={
+  "cards": [
+    {
+      "text": "test\n",
+      "audio": {
+        "converting": false,
+        "name": "rec1393744046080.webm",
+        "startTime": "2014-03-02T07:07:26.080Z",
+        "stopTime": "2014-03-02T07:07:37.857Z",
+        "path": "media/rec1393744046080.webm"
+      },
+      "image": {
+        "name": "garden_icon.png",
+        "path": "media/garden_icon.png"
+      },
+      "formattedText": "<p>test</p>"
+    },
+    {
+      "image": {},
+      "audio": {}
+    }
+  ],
+  "name": "slide-show-pub-test"
+}
