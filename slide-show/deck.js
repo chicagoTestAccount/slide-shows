387274ef192331c06e2e45ae56e3721521d7b518
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 22:43:36 -0800

    Show & Tell commit

--- slide-show/deck.js
+++ slide-show/deck.js
@@ -1,32 +1,10 @@
 var deck={
   "cards": [
     {
-      "text": "#2\n",
-      "audio": {
-        "converting": false,
-        "name": "rec1393741900671.webm",
-        "startTime": "2014-03-02T06:31:40.671Z",
-        "stopTime": "2014-03-02T06:31:49.125Z",
-        "path": "media/rec1393741900671.webm"
-      },
-      "formattedText": "<h1>2</h1>",
-      "image": {}
-    },
-    {
-      "text": "* a\n* b\n* c",
-      "formattedText": "<ul>\n<li>a</li>\n<li>b</li>\n<li>c</li>\n</ul>",
+      "text": "Incremental saving test",
+      "formattedText": "<p>Incremental saving test</p>",
       "image": {},
       "audio": {}
-    },
-    {
-      "audio": {
-        "converting": false,
-        "name": "rec1393742114320.webm",
-        "startTime": "2014-03-02T06:35:14.320Z",
-        "stopTime": "2014-03-02T06:35:22.678Z",
-        "path": "media/rec1393742114320.webm"
-      },
-      "image": {}
     }
   ],
   "name": "slide-show"
