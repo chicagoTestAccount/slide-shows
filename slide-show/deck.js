cba152dfef3d5a416d7bdb1a744d447dea55d44e
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 22:31:57 -0800

    Show & Tell commit

---
+++ slide-show/deck.js
@@ -0,0 +1,23 @@
+var deck={
+  "cards": [
+    {
+      "text": "#2\n",
+      "audio": {
+        "converting": false,
+        "name": "rec1393741900671.webm",
+        "startTime": "2014-03-02T06:31:40.671Z",
+        "stopTime": "2014-03-02T06:31:49.125Z",
+        "path": "media/rec1393741900671.webm"
+      },
+      "formattedText": "<h1>2</h1>",
+      "image": {}
+    },
+    {
+      "text": "* a\n* b\n* c",
+      "formattedText": "<ul>\n<li>a</li>\n<li>b</li>\n<li>c</li>\n</ul>",
+      "image": {},
+      "audio": {}
+    }
+  ],
+  "name": "slide-show"
+}
