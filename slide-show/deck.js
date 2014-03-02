4ed622fadcabda2af157dc540b2d12ac9cb3b16c
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 22:43:56 -0800

    Show & Tell commit

--- slide-show/deck.js
+++ slide-show/deck.js
@@ -1,8 +1,14 @@
 var deck={
   "cards": [
     {
-      "text": "Incremental saving test",
+      "text": "Incremental saving test\n",
       "formattedText": "<p>Incremental saving test</p>",
+      "image": {},
+      "audio": {}
+    },
+    {
+      "text": "new card",
+      "formattedText": "<p>new card</p>",
       "image": {},
       "audio": {}
     }
