642312c30c241fb32f71919079b5a692d24236d0
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 22:38:41 -0800

    Show & Tell commit

---
+++ custom-name/deck.js
@@ -0,0 +1,15 @@
+var deck={
+  "cards": [
+    {
+      "audio": {
+        "converting": false,
+        "name": "rec1393742296039.webm",
+        "startTime": "2014-03-02T06:38:16.039Z",
+        "stopTime": "2014-03-02T06:38:25.963Z",
+        "path": "media/rec1393742296039.webm"
+      },
+      "image": {}
+    }
+  ],
+  "name": "custom-name"
+}
