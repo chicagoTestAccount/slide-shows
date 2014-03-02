12bdeea92330402bb93b8e7ce5b1447d304c99f6
chicagoTestAccount <nbnate@uw.edu>
2014-03-01 22:40:53 -0800

    Show & Tell commit

--- custom-name/deck.js
+++ custom-name/deck.js
@@ -8,8 +8,8 @@ var deck={
         "stopTime": "2014-03-02T06:38:25.963Z",
         "path": "media/rec1393742296039.webm"
       },
-      "text": "* cu\n** st\n*** om\n* na\n** me\n*** s",
-      "formattedText": "<ul>\n<li>cu\n** st\n<em>*</em> om</li>\n<li>na\n** me\n<em>*</em> s</li>\n</ul>",
+      "text": "* cu\n* * st\n* * * om\n* na\n* * me\n* * * s",
+      "formattedText": "<ul>\n<li>cu</li>\n<li><ul><li>st</li></ul></li>\n<li><ul><li><ul><li>om</li></ul></li></ul></li>\n<li>na</li>\n<li><ul><li>me</li></ul></li>\n<li><ul><li><ul><li>s</li></ul></li></ul></li>\n</ul>",
       "image": {}
     }
   ],
