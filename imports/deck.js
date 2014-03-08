87e693f0974f22b270441a77496dad5c57ba1774
chicagoTestAccount <nbnate@uw.edu>
2014-03-08 01:43:38 -0800

    Show & Tell commit

---
+++ imports/deck.js
@@ -0,0 +1,24 @@
+var deck={
+  "cards": [
+    {
+      "text": "# Imports work",
+      "formattedText": "<h1>Imports work</h1>",
+      "image": {},
+      "audio": {}
+    },
+    {
+      "audio": {
+        "converting": false,
+        "name": "rec1394271784072.webm",
+        "startTime": "2014-03-08T09:43:04.072Z",
+        "stopTime": "2014-03-08T09:43:12.117Z",
+        "path": "media/rec1394271784072.webm"
+      },
+      "image": {
+        "name": "SAT logo.png",
+        "path": "media/SAT logo.png"
+      }
+    }
+  ],
+  "name": "imports"
+}
